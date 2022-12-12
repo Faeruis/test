1791c0df135fa3faf5a9e23d776f18fb30eb03a7
Tristan <faeruispro@gmail.com>
2022-12-12 22:54:50 +0100

    update

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
 def max(a,b):
     if a>b:
         return a
     else:
         return b
-=======
 def min(a,b):
     if a<b:
         return a
     else:
         return b
 
+def helloworld():
+    print("hello world")
+
 def main():
     print(max(2,3))
     print(min(2,3))
 
 main()
->>>>>>> refs/remotes/origin/main
